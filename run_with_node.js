d50fd23affab617d649f847a4e1122be4ac2553a
Oluwayomi Oke <okeoluwayomi9@gmail.com>
2021-09-27 18:14:47 +0100

    Add files via upload

---
+++ run_with_node.js
@@ -0,0 +1,9 @@
+// load dependencies
+require("./code/load")("code/journal.js", "code/data.js");
+
+for (let event of journalEvents(JOURNAL)) {
+  let correlation = phi(tableFor(event, JOURNAL));
+  if (correlation > 0.1 || correlation < -0.1) {
+    console.log(event + ":", correlation);
+  }
+}
